2267214518699f2ebce2d9d0ae5762b72555b0bf
Matt Harasymczuk <matt@astrotech.io>
2019-03-19 09:04:44 +0100

    Book: Config #time 10m

--- book/conf.py
+++ book/conf.py
@@ -15,10 +15,38 @@ extensions = [
 
 todo_emit_warnings = False
 todo_include_todos = True
-html_theme = 'sphinx_rtd_theme'
-pygments_style = 'vs'
+exclude_patterns = []
 
-language = 'en'
+# -----------------------------------------------------------------------------
+# Standard book config
+# -----------------------------------------------------------------------------
+
+import datetime
+import os
+import re
+import subprocess
+import sys
+
+
+html_theme = 'sphinx_rtd_theme'
+
+exclude_patterns = exclude_patterns + [
+    '*/_template.rst',
+    '*/solution/*',
+    '.*',
+    'venv*',
+    'virtualenv*',
+    '_extensions',
+    '_img',
+    '_slides',
+    '_static',
+    '_themes',
+    '_tmp',
+    '*/solutions/*',
+    '**.ipynb_checkpoints',
+    'README.rst'
+]
+
 numfig_format = {
     'section': 'Sect. %s.',
     'figure': 'Fig. %s.',
@@ -26,36 +54,40 @@ numfig_format = {
     'code-block': 'Code Listing %s.',
 }
 
-# -----------------------------------------------------------------------------
-# Unified book config
-# -----------------------------------------------------------------------------
+language = 'en'
+source_directory = '.'
+master_doc = 'index'
 highlight_language = 'python3'
+pygments_style = 'vs'
 numfig = True
 templates_path = ['_templates']
-master_doc = 'index'
 source_suffix = ['.rst']
 imgmath_image_format = 'svg'
 today_fmt = '%Y-%m-%d'
-project_slug = project.lower().replace(' ', '-')
+
+project_slug = re.sub(r'[\W]+', '', project)
 sha1 = subprocess.Popen('git log -1 --format="%h"', stdout=subprocess.PIPE, shell=True).stdout.read().decode().replace('\n', '')
-version = '#{sha1}, {date:%Y-%m-%d}'.format(sha1=sha1, date=datetime.date.today())
-release = '#{sha1}, {date:%Y-%m-%d}'.format(sha1=sha1, date=datetime.date.today())
-copyright = '{year}, {author} <{email}>'.format(
-    year=datetime.date.today().year,
-    author=author,
-    email=email,
-)
+today = datetime.date.today()
+version = f'#{sha1}, {today:%Y-%m-%d}'
+release = f'#{sha1}, {today:%Y-%m-%d}'
+copyright = f'{today:%Y}, {author} <{email}>'
 
-exclude = ['README.rst']
-exclude_patterns = ['_build', '_book', '_slides', 'img', '_themes', 'slides', 'README.rst', '*/_template.rst']
+extensions_dir = os.path.join(os.path.dirname(__file__), '', '_extensions')
+sys.path.append(extensions_dir)
 
+htmlhelp_basename = project
 html_theme_path = ['_themes']
-html_static_path = []
+html_static_path = ['_static']
+html_favicon = '_static/favicon.png'
 html_sidebars = {'sidebar': ['localtoc.html', 'sourcelink.html', 'searchbox.html']}
 html_show_sphinx = False
-htmlhelp_basename = project
+html_context = {
+    'css_files': [
+        '_static/theme-overrides.css',
+    ],
+}
 
-latex_documents = [(master_doc, '{0}.tex'.format(project_slug), project, author, 'manual')]
+latex_documents = [(master_doc, f'{project_slug}.tex', project, author, 'manual')]
 latex_elements = {
     'papersize': 'a4paper',
     'pointsize': '10pt',
@@ -73,7 +105,6 @@ epub_author = author
 epub_publisher = author
 epub_copyright = copyright
 epub_exclude_files = ['search.html']
-
 
 man_pages = [
     (master_doc, project_slug, project, [author], 1)
